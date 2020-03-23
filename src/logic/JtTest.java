41e456d23ef548cf2ce44638448a426ea33dbee1
AlBaldo <61839652+AlBaldo@users.noreply.github.com>
2020-03-23 11:59:49 +0100

    Changed Sys.out with logger

--- src/logic/JtTest.java
+++ src/logic/JtTest.java
@@ -1,8 +1,12 @@
 package logic;
 
+import java.util.logging.Logger;
+
 public class JtTest {
 	
 	public static void main(String[] arg) {
-		System.out.println("Hello Travis!");
+		//System.out.println("Hello Travis!");
+		Logger mlog = Logger.getLogger(Logger.GLOBAL_LOGGER_NAME);
+		mlog.info("Hello travis!"); 
 	}
 }
