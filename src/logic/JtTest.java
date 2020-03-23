39172b546314e7b8194d5ce362ca2ce131be86b4
AlBaldo <61839652+AlBaldo@users.noreply.github.com>
2020-03-23 12:06:24 +0100

    removed commented code

--- src/logic/JtTest.java
+++ src/logic/JtTest.java
@@ -5,7 +5,7 @@ import java.util.logging.Logger;
 public class JtTest {
 	
 	public static void main(String[] arg) {
-		//System.out.println("Hello Travis!");
+
 		Logger mlog = Logger.getLogger(Logger.GLOBAL_LOGGER_NAME);
 		mlog.info("Hello travis!"); 
 	}
